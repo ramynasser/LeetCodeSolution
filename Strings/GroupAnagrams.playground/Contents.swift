43ba6bd2c4604cfafddb7b782d2768aad0e2006e
Ramy Nasser <ramy.assr@vodafone.com>
2020-07-20 12:20:50 +0200

    # implement solution for problem #49 in Leetcode

--- Strings/GroupAnagrams.playground/Contents.swift
+++ Strings/GroupAnagrams.playground/Contents.swift
@@ -1,17 +1,33 @@
-
+/**
+* Problem
+*     49.Group Anagrams
+*     https://leetcode.com/problems/group-anagrams/
+*     https://leetcode.com/problems/group-anagrams/
+* Grade of difficulty
+*     Medium
+* Related topics
+* @author Ramy
+*/
 import Foundation
-func findDuplicates(_ nums: [Int]) -> [Int] {
-    var emptyIntSet:Set = Set<Int>()
-    var result:[Int] = []
-    for num in  nums {
-       if(nums[num - 1] > 0){
-            nums[num - 1] *= -1
-        } else{
-            res.add(Math.abs(num));
+class Solution {
+ func groupAnagrams(_ strs: [String]) -> [[String]] {
+    var tableData = [String: Int]()
+    var results = [[String]]()
+    
+    for str in strs {
+        let arr = Array(str)
+        
+        let sortedString = String(arr.sorted())
+        if let id = tableData[sortedString] {
+            results[id].append(str)
+        } else {
+            results.append([str])
+            tableData[sortedString] = results.count-1
         }
     }
     
-    return result
+    return results
 }
-let a = findDuplicates([4,3,2,7,8,2,3,1])
-print(a)
+
+}
+
