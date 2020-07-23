e81620d16a7f0387a0ddabc1d086f51aac6e7f1f
ramy nasser <ramynasser95@gmail.com>
2020-07-24 00:18:56 +0200

    solve challenge 94. Binary Tree Inorder Traversal

---
+++ BinaryTree/Binary Tree Inorder Traversal.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+/**
+* Problem
+*     94. Binary Tree Inorder Traversal
+*     https://leetcode.com/problems/binary-tree-inorder-traversal/
+* Grade of difficulty
+*     Medium
+* Related topics
+* @author Ramy
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func inorderTraversal(_ root: TreeNode?) -> [Int] {
+         if root == nil {
+              return []
+         }
+      var result: [Int] = []
+      result += inorderTraversal(root?.left)
+      result.append(root!.val)
+      result += inorderTraversal(root?.right)
+      return result
+    }
+}
